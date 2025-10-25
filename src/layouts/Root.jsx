ed1691992992d01c44b842d85a2d13b7e64afb94
MD Arman khan <mdarmankhan6252@gmail.com>
2025-10-26 04:03:31 +0600

    I have added

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -4,30 +4,34 @@ import Footer from "../components/Footer";
 import ClickSpark from '../lib/ClickSpark/ClickSpark';
 import light_1 from '../assets/light-1.png'
 import light_2 from '../assets/light-2.png'
+import ScrollToTop from "../components/ScrollToTop";
 
 const Root = () => {
    return (
-      <ClickSpark
-         sparkColor='#06b1b1'
-         sparkSize={14}
-         sparkRadius={25}
-         sparkCount={9}
-         duration={400}
-         extraScale={1.5}
-      >
-         <div className="bg-gradient-to-tr from-[#22001f] via-[#081200] to-[#0b0120] w-full min-h-screen text-white py-4">
-            <div className="max-w-6xl mx-auto px-3 sm:px-5">
-               <Nav />
-               <div className="">
-                  <Outlet />
+      <div>
+         <ScrollToTop />
+         <ClickSpark
+            sparkColor='#06b1b1'
+            sparkSize={14}
+            sparkRadius={25}
+            sparkCount={9}
+            duration={400}
+            extraScale={1.5}
+         >
+            <div className="bg-gradient-to-tr from-[#22001f] via-[#081200] to-[#0b0120] w-full min-h-screen text-white py-4">
+               <div className="max-w-6xl mx-auto px-3 sm:px-5">
+                  <Nav />
+                  <div className="">
+                     <Outlet />
+                  </div>
+                  <Footer />
                </div>
-               <Footer />
-            </div>
 
-            <img src={light_1} alt="" className="absolute left-0 top-0 opacity-50 w-[400px] hidden 2xl:inline-block" />
-            <img src={light_2} alt="" className="absolute right-0 top-0 opacity-50 w-[400px] hidden 2xl:inline-block" />
-         </div>
-      </ClickSpark>
+               <img src={light_1} alt="" className="absolute left-0 top-0 opacity-50 w-[400px] hidden 2xl:inline-block" />
+               <img src={light_2} alt="" className="absolute right-0 top-0 opacity-50 w-[400px] hidden 2xl:inline-block" />
+            </div>
+         </ClickSpark>
+      </div>
    );
 };
 
